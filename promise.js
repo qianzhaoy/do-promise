974f7dad98bf533687b53c83303ab023bb3ec102
qianzhaoyan <779547508@qq.com>
2018-04-04 00:34:07 +0800

    提交

---
+++ promise.js
@@ -0,0 +1,63 @@
+(function (window) {
+
+    function promise(func) {
+        this.status = null;
+        this.data = null;
+        this.queue = [];
+        this.failCb = null;
+        func && func.call(this, promise.resolve.bind(this), promise.reject.bind(this))
+        return this;
+    }
+
+    promise.resolve = function (data) {
+        var context = this;
+        context.status = 'success';
+        context.data = data;
+        var func = context.queue.shift();
+        if(func){
+            try{
+                var d = func(data);
+                if(d instanceof promise){
+                    d = d.data;
+                }
+                promise.resolve.call(context, d);
+            }catch(err){
+                promise.reject.call(context, err);
+            }
+        }
+    }
+
+    promise.reject = function (data) {
+        this.status = 'fail';
+        this.data = data;
+        if(this.failCb){
+            this.failCb(data)
+        }else{
+            throw new Error("promise catch")
+        }
+    }
+
+    promise.prototype.then = function (callback) {
+        var pass = typeof callback === "function";
+        if(this.status === 'success'){
+            this.data = pass ? callback(this.data) : void 0;
+        }else if(this.status === null){
+            pass && this.queue.push(callback);
+        }
+        
+        return this;
+    }
+
+    promise.prototype.catch = function (callback) {
+        if (this.status === 'fail') {
+            this.data = callback && callback(this.data);
+        }else if(this.status === null){
+            this.failCb = callback
+        }
+        return this;
+    }
+
+
+    window.promise = promise;
+
+}(window, undefined))
