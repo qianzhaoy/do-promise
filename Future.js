7a1a010c1af5ed3bf78e582c5c3bcb525102afad
qianzhaoyan <779547508@qq.com>
2018-04-05 23:12:39 +0800

    bug修复

--- Future.js
+++ Future.js
@@ -1,65 +1,70 @@
 (function (window) {
 
-    function promise(func) {
-        this.status = "pending";
+    var RESOLVE = "resolve";
+    var REJECT = "reject";
+    var PENDING = "pending";
+
+    function Future(func) {
+        var _this = this;
+        this.status = PENDING;
         this.data = null;
         this.queue = [];
         this.failCb = null;
         setTimeout(function(){
-            func && func.call(this, promise.resolve.bind(this), promise.reject.bind(this))
+            func && func.call(this, Future.resolve.bind(_this), Future.reject.bind(_this))
         }, 0);
         return this;
     }
 
-    promise.resolve = function (data) {
+    Future.resolve = function (data) {
         var context = this;
-        context.status = 'resolve';
+        context.status = RESOLVE;
         context.data = data;
         var func = context.queue.shift();
         if(func){
             try{
                 var d = func(data);
-                if(d instanceof promise){
+                if(d instanceof Future){
                     d = d.data;
                 }
-                promise.resolve.call(context, d);
+                Future.resolve.call(context, d);
             }catch(err){
-                promise.reject.call(context, err);
+                Future.reject.call(context, err);
             }
         }
     }
 
-    promise.reject = function (data) {
-        this.status = 'reject';
+    Future.reject = function (data) {
+        this.status = REJECT;
         this.data = data;
         if(this.failCb){
             this.failCb(data)
         }else{
-            throw new Error("promise catch")
+            throw new Error("Future catch")
         }
     }
 
-    promise.prototype.then = function (callback) {
+    Future.prototype.then = function (callback) {
         var pass = typeof callback === "function";
-        if(this.status === 'success'){
+        if(this.status === RESOLVE){
             this.data = pass ? callback(this.data) : void 0;
-        }else if(this.status === null){
+        }else if(this.status === PENDING){
             pass && this.queue.push(callback);
         }
         
         return this;
     }
 
-    promise.prototype.catch = function (callback) {
-        if (this.status === 'fail') {
+    Future.prototype.catch = function (callback) {
+        if (this.status === REJECT) {
             this.data = callback && callback(this.data);
-        }else if(this.status === null){
+        }else if(this.status === PENDING){
             this.failCb = callback
         }
         return this;
     }
 
 
-    window.promise = promise;
+    window.Future = Future;
 
 }(window, undefined))
