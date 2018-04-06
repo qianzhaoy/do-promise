70302b8957378e02201309e643ea9e29d0da6776
qianzhaoyan <779547508@qq.com>
2018-04-06 15:32:55 +0800

    脑细胞死光,完成版

--- Future.js
+++ Future.js
@@ -10,26 +10,25 @@
         this.data = null;
         this.queue = [];
         this.failCb = null;
-        setTimeout(function(){
+        setTimeout(function () {
             func && func.call(this, Future.resolve.bind(_this), Future.reject.bind(_this))
         }, 0);
-        return this;
     }
 
     Future.resolve = function (data) {
-        var context = this;
-        context.status = RESOLVE;
-        context.data = data;
-        var func = context.queue.shift();
-        if(func){
-            try{
-                var d = func(data);
-                if(d instanceof Future){
+        this.status = RESOLVE;
+        this.data = data;
+        
+        for (let i = 0; i < this.queue.length; i++) {
+            var obj = this.queue[i];
+            try {
+                var d = obj.cb(data);
+                if (d instanceof Future) {
                     d = d.data;
                 }
-                Future.resolve.call(context, d);
-            }catch(err){
-                Future.reject.call(context, err);
+                Future.resolve.call(obj.future, d);
+            } catch (err) {
+                Future.reject.call(obj.future, err);
             }
         }
     }
@@ -37,28 +36,53 @@
     Future.reject = function (data) {
         this.status = REJECT;
         this.data = data;
-        if(this.failCb){
+        if (this.failCb) {
             this.failCb(data)
-        }else{
-            throw new Error("Future catch")
+        } else {
+            throw new Error("Future need catch")
         }
     }
 
     Future.prototype.then = function (callback) {
-        var pass = typeof callback === "function";
-        if(this.status === RESOLVE){
-            this.data = pass ? callback(this.data) : void 0;
-        }else if(this.status === PENDING){
-            pass && this.queue.push(callback);
-        }
+        if (typeof callback !== "function") throw new Error("then函数的参数必须为函数");
         
-        return this;
+        var thatFuture = new Future(function(){})
+        
+        switch (this.status) {
+            case RESOLVE:
+                try{
+                    var d = callback(this.data);
+                    Future.resolve.call(thatFuture, d);
+                }catch(err){
+                    Future.reject.call(thatFuture, err)
+                }
+                break;
+                    
+            case PENDING:
+                this.queue.push({
+                    future: thatFuture,
+                    cb: callback
+                });
+                
+                break;
+
+            default:
+                Future.reject.call(thatFuture, this.data)
+                break;
+        }
+        // if (this.status === RESOLVE) {
+        //     this.data = callback(this.data);
+        // } else if (this.status === PENDING) {
+        //     this.queue.push(callback);
+        // }
+
+        return thatFuture;
     }
 
     Future.prototype.catch = function (callback) {
         if (this.status === REJECT) {
             this.data = callback && callback(this.data);
-        }else if(this.status === PENDING){
+        } else if (this.status === PENDING) {
             this.failCb = callback
         }
         return this;
